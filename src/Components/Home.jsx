96438f5a8863c8699b1fcd047fb7af93fbdd359f
Amogh Madireddi <amoghmm@umich.edu>
2024-11-21 03:24:26 +0000

    changed background

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/ground.jpg";
+import image from "../images/white.jpg";
 
-const imageAltText = "cool ground";
+const imageAltText = "white";
 
 const Home = ({ name, title }) => {
   return (
